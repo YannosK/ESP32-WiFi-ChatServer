843d4378f1a3eae6773d6ca3e7c59cfc1bf3a0e6
YannosK <giannos2405@gmail.com>
2023-10-01 01:06:13 +0300

    A chat server between ESP32 abd RPI 3

---
+++ rpi-python/TCPserver.py
@@ -0,0 +1,49 @@
+import socket
+
+# Define server address and port
+host = '0.0.0.0'  # Listen on all available interfaces
+port = 80
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific address and port
+server_socket.bind((host, port))
+
+# Listen for incoming connections (max queued connections is 5)
+server_socket.listen(5)
+print(f"Server listening on {host}:{port}")
+
+while True:
+    # Wait for a connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+
+    # Send a welcome message to the client
+    welcome_message = "Welcome to the chat server! Type 'bye' to exit."
+    client_socket.send(welcome_message.encode())
+
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024)
+        #if not data:
+        #    break
+
+        # Convert bytes to string
+        received_message = data.decode()
+
+        # Print the received message
+        print(f"Received message from {client_address}: {received_message}")
+
+        # Check if the client wants to exit
+        if received_message.lower() == 'bye':
+            break
+
+        # Get a response from the server user
+        response_message = input("Your response: ")
+
+        # Send the response back to the client
+        client_socket.send(response_message.encode())
+
+    print(f"Connection with {client_address} closed.")
+    client_socket.close()
---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+
+void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD);
+void ConnectToServer(IPAddress server);
+
+int status = WL_IDLE_STATUS;
+
+IPAddress server(192, 168, 1, 44);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+
+  char WiFi_ssid[32] = "HOME_EXT";
+  char WiFi_pswd[32] = "123456789";
+
+  ConnectToWiFi(WiFi_ssid, WiFi_pswd);
+
+  ConnectToServer(server);
+}
+
+
+void loop() {
+
+  while(client.connected()){
+    // if there are incoming bytes available from the server, read them and print them:
+    while(client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }
+
+    while(Serial.available())
+    {
+      String msg_to_client = Serial.readString();
+      client.print(msg_to_client);
+    }   
+  }
+
+  Serial.println();
+  Serial.println("disconnecting from server.");
+  //client.stop();
+
+  while(true)
+  {}
+}
+
+
+void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD) {
+  Serial.print("Connecting to WiFi");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("Connection failed!");
+  }
+  else{
+    Serial.print("\nConected to WiFi network with local IP address:");
+    Serial.println(WiFi.localIP()); 
+  }
+}
+
+
+void ConnectToServer(IPAddress server)
+{
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  client.connect(server, 80);
+
+  //affirmation that we connected indeed
+  if (client.connected()) {
+    Serial.println("connected to server");
+  }  
+}
