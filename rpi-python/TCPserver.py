56ce215cee4eb5586873e7afdcac84e6358d763b
Panagiotis Papaioannou <ppapaioannou1998@outlook.com>
2023-10-01 21:33:04 +0300

    testing

--- rpi-python/TCPserver.py
+++ rpi-python/TCPserver.py
@@ -21,7 +21,52 @@ def init():
     print(f"Server listening on {host}:{port}")
 
 def loop():
-     while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+    led.on() # Turn LED on
+    
+     # Send a welcome message to the client
+    welcome_message = "Welcome to the chat server! Type 'bye' to exit.!"
+    client_socket.send(welcome_message.encode())
+
+    print(f"Type a message to start a conversation with {client_address}")
+    
+    while True:
+        
+        try:
+            # Check if there is data available for reading from the client socket
+            readable, _, _ = select.select([client_socket], [], [], 0.1)
+            
+            if readable:
+                # Receive data from the client
+                data = client_socket.recv(1024)
+
+                if not data:
+                    print(f"Connection with {client_address} closed.")
+                    break
+
+                # Convert bytes to string
+                received_message = data.decode()
+
+                # Print the received message
+                print(f"Received message from {client_address}: {received_message}")
+
+                # Check if the client wants to exit
+                if received_message.lower() == 'bye':
+                    print(f"Connection with {client_address} closed.")
+                    break
+
+                # Get a response from the server user
+                response_message = input("Your response: ")
+
+                # Send the response back to the client
+                client_socket.send(response_message.encode())
+
+        except socket.error:
+            # Handle socket errors (e.g., client disconnects)
+            print(f"Connection with {client_address} closed.")
+            break
+        '''
         # Wait for a connection
         client_socket, client_address = server_socket.accept()
         print(f"Accepted connection from {client_address}")
@@ -47,6 +92,7 @@ def loop():
         print(f"Connection with {client_address} closed.")
         client_socket.close()
         led.off() # Turn LED off
+        '''
 
 
 def clientRead(client_socket, client_address):
