5ddfd7896bf1f32afc149b90c38c67d8e5943712
Panagiotis Papaioannou <ppapaioannou1998@outlook.com>
2023-10-01 21:26:46 +0300

    major changes to file, methods and readable

--- rpi-python/TCPserver.py
+++ rpi-python/TCPserver.py
@@ -1,47 +1,59 @@
 import socket
+import select
 import gpiozero  # The GPIO library for Raspberry Pi
-import time  # Enables Python to manage timing
 
-# define the rpi GPIO
+ # define the rpi GPIO
 led = gpiozero.LED(26) # Reference GPIO26
 
-# Define server address and port
-host = '0.0.0.0'  # Listen on all available interfaces
-port = 80
-
-# Create a socket object
+# define a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to a specific address and port
-server_socket.bind((host, port))
+def init():
+    # Define server address and port
+    host = '0.0.0.0'  # Listen on all available interfaces
+    port = 80
 
-# Listen for incoming connections (max queued connections is 5)
-server_socket.listen(5)
-print(f"Server listening on {host}:{port}")
+    # Bind the socket to a specific address and port
+    server_socket.bind((host, port))
+
+    # Listen for incoming connections (max queued connections is 5)
+    server_socket.listen(5)
+    print(f"Server listening on {host}:{port}")
+
+def loop():
+     while True:
+        # Wait for a connection
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+        led.on() # Turn LED on
+
+        # Send a welcome message to the client
+        welcome_message = "Welcome to the chat server! Type 'bye' to exit.!"
+        client_socket.send(welcome_message.encode())
+
+        print(f"Type a message to start a conversation with {client_address}")
+
+        
+
+        #while True:
+        # Check if there is data available for reading from the client socket
+        readable, _, _ = select.select([client_socket], [], [], 0.1)
+
+        if not readable :
+            replyToClient(client_socket, readable)
+             
+        clientRead(client_socket)
+
+        print(f"Connection with {client_address} closed.")
+        client_socket.close()
+        led.off() # Turn LED off
 
 
-while True:
-    # Wait for a connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address}")
-    led.on() # Turn LED on
-
-    # Send a welcome message to the client
-    welcome_message = "Welcome to the chat server! Type 'bye' to exit.!"
-    client_socket.send(welcome_message.encode())
-
-    print(f"Type a message to start a conversation with {client_address}")
-
-    # Send message to the client
-    response_message = input("Your message: ")
-    client_socket.send(response_message.encode())
-
-    while True:
-        # Receive data from the client
+def clientRead(client_socket):
+    # Receive data from the client
         data = client_socket.recv(1024)
-        #led.on() # Turn LED on
-        if not data:
-            break
+        #if not data:
+        #    break
 
         # Convert bytes to string
         received_message = data.decode()
@@ -50,23 +62,27 @@ while True:
         print(f"Received message from {client_address}: {received_message}")
 
         # Check if the client wants to exit
-        if received_message.lower() == 'bye':
-            break
+        #if received_message.lower() == 'bye':
+        #    break
 
+def replyToClient(client_socket, readable):
+    if readable:
+         return
+    else:    
         # Get a response from the server user
-        response_message = input("Your response: ")
+        response_message = input("Your message: ")
 
         # Send the response back to the client
         client_socket.send(response_message.encode())
 
 
 
-    print(f"Connection with {client_address} closed.")
-    client_socket.close()
-    led.off() # Turn LED off
+
+init()
+loop()
+
 
     
-
 
 
 
